58d16befa857fc8757ce0e9f7d190995e528c4ad
Builder <builder@example.com>
2026-03-28 16:03:14 +0800

    Fix resource linking errors
    Changes:
    1. Remove mipmap/ic_launcher references (resources don't exist)
    2. Remove usesCleartextForLocalization (not supported)
    3. Simplify MainActivity (remove unused imports and setContentView)
    4. Remove activity_main.xml layout (not needed for Xposed module)
    5. Use hardcoded app name instead of @string/app_name
    
    This fixes AAPT errors:
    - resource mipmap/ic_launcher not found
    - resource mipmap/ic_launcher_round not found
    - attribute usesCleartextForLocalization not found

--- device-module/app/src/main/java/com/wireless/control/device/MainActivity.kt
+++ device-module/app/src/main/java/com/wireless/control/device/MainActivity.kt
@@ -1,24 +1,11 @@
 package com.wireless.control.device
 
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
-import com.wireless.control.device.server.DeviceControlServer
-import com.wireless.control.device.service.DeviceControlService
-import com.wireless.control.device.service.DeviceMonitorService
-import com.wireless.control.device.service.HeartbeatService
-import com.wireless.control.device.service.DeviceAccessibilityService
-import com.wireless.control.device.service.NotificationListenerService
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 /**
  * 设备控制主Activity
  */
-
 class MainActivity : AppCompatActivity() {
 
     companion object {
@@ -35,9 +22,7 @@ class MainActivity : AppCompatActivity() {
         instance = this
 
         try {
-            setContentView(R.layout.activity_main)
             Log.i(TAG, "MainActivity created")
-            startServices()
         } catch (e: Exception) {
             Log.e(TAG, "Failed to initialize MainActivity", e)
         }
@@ -46,29 +31,5 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         instance = null
-    }
-
-    private fun startServices() {
-        lifecycleScope.launch(Dispatchers.Main) {
-            try {
-                // 启动设备控制服务
-                val controlIntent = Intent(this@MainActivity, DeviceControlService::class.java)
-                startForegroundService(controlIntent)
-                Log.i(TAG, "Device control service started")
-
-                // 启动监控服务
-                val monitorIntent = Intent(this@MainActivity, DeviceMonitorService::class.java)
-                startForegroundService(monitorIntent)
-                Log.i(TAG, "Device monitor service started")
-
-                // 启动心跳服务
-                val heartbeatIntent = Intent(this@MainActivity, HeartbeatService::class.java)
-                startForegroundService(heartbeatIntent)
-                Log.i(TAG, "Heartbeat service started")
-
-            } catch (e: Exception) {
-                Log.e(TAG, "Failed to start services", e)
-            }
-        }
     }
 }
